a944559b36e6fe02b4a1e6ad529ec72627a01dcd
prithi_pal_singh_prithi_pal_singh@sfu.ca <pprithip@sfu.ca>
2018-11-05 05:27:28 +0000

    Corrected iteration. still not completes epoch

--- hw3/default.py
+++ hw3/default.py
@@ -63,38 +63,50 @@ def perc_train(train_data, tagset, numepochs):
             if sent_ind==0 : 
                 GLOBAL_DIC=FEATURE_DIC
 
+            
             argmax_tag=perc.perc_test(GLOBAL_DIC, sent_labels, sent_features, tagset, default_tag)           
+            
+            
             
             ## for each word
             for w,i in zip(sent_labels,range(len(sent_labels))) : 
+                
+                
                 
                 (feat_index, feats) = perc.feats_for_word(i, sent_features)    
                 actual_tag=re.findall(r"([A-Z\-]*)$",w)[0]
                 word=re.findall(r"^([A-Za-z]*) *",w)[0]
                 #print("word_index = {}, word = {}, actual_tag = {}, argmax_tag= {}".format(i,word,actual_tag,argmax_tag[i]))
                
+                ## for each (word_feature,true_tag)
+                local_keys=FEATURE_DIC.keys()
+                global_keys=GLOBAL_DIC.keys()
                 
                 for (f,t) in itertools.product(feats,[actual_tag]) : 
                     #print("({},{}) {}".format(f,t,argmax_tag[i]))
                     if t!= argmax_tag[i] : 
-                        #print("yes")   
-                        if (f,t) in FEATURE_DIC.keys() and (f,argmax_tag[i]) in FEATURE_DIC.keys() : 
+                        #print("yes")  
+                        if (f,t) in local_keys : 
                             FEATURE_DIC[(f,t)]=FEATURE_DIC[f,t]+1
-                            FEATURE_DIC[(f,argmax_tag[i])]=FEATURE_DIC[(f,argmax_tag[i])]-1
-                            
                         else : 
-                            FEATURE_DIC[(f,t)]=1
+                            FEATURE_DIC[(f,t)]=1  
+                        if (f,argmax_tag[i]) in local_keys : 
+                            FEATURE_DIC[(f,argmax_tag[i])]=FEATURE_DIC[(f,argmax_tag[i])]-1
+                        else : 
                             FEATURE_DIC[(f,argmax_tag[i])]=-1
                 
             #print(list(GLOBAL_DIC.values())[:10])
                             
             ## appending to global feature vector
             
-           
+            local_keys=FEATURE_DIC.keys()
+            global_keys=GLOBAL_DIC.keys()
+            
+            t_start=time.time()
             if sent_ind!=0 : 
                 ## appending new features of current feature vector if not already in global
-                for k in list(FEATURE_DIC.keys()) : 
-                    global_k=list(GLOBAL_DIC.keys())
+                for k in list(local_keys) : 
+                    global_k=list(global_keys)
                     if k not in global_k : 
                         if FEATURE_DIC[k]!=0 : 
                             GLOBAL_DIC[k]=FEATURE_DIC[k]
@@ -102,12 +114,12 @@ def perc_train(train_data, tagset, numepochs):
                         if GLOBAL_DIC[k]+FEATURE_DIC[k]!=0 : 
                             GLOBAL_DIC[k]=GLOBAL_DIC[k] + FEATURE_DIC[k]
             
-            print("sentence {} trained".format(sent_ind))
+            print("sentence {} trained in {}".format(sent_ind,time.time()-t_start))
             sent_ind=sent_ind+1
             
-        
+            
         e=e+1
-        
+
         
     print("Training finished in ",time.time()-t1)
     return GLOBAL_DIC
