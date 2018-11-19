1ffd4b1cf5b3975cc3bc698ff948d993429b3506
prithi_pal_singh_prithi_pal_singh@sfu.ca <pprithip@sfu.ca>
2018-11-19 05:30:56 +0000

    added Smoothening. New AER 0.3 @ 5 iterations

--- hw4/baseline.py
+++ hw4/baseline.py
@@ -12,8 +12,10 @@ optparser.add_option("-l", "--logfile", dest="logfile", default=None, help="file
 optparser.add_option("-t", "--threshold", dest="threshold", default=5, type="float", help="threshold for alignment (default=0.5)")
 optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxsize, type="int", help="Number of sentences to use for training and alignment")
 (opts, _) = optparser.parse_args()
+
 f_data = "%s.%s" % (os.path.join(opts.datadir, opts.fileprefix), opts.french)
 e_data = "%s.%s" % (os.path.join(opts.datadir, opts.fileprefix), opts.english)
+e_with_null_data= "%s.%s" % (os.path.join(opts.datadir, '{}_null'.format(opts.fileprefix)), opts.english)
 
 
 verbose=True
@@ -25,8 +27,12 @@ if opts.logfile:
 
 if verbose:
     sys.stderr.write("Training with Dice's coefficient...\n")
+## Bitext is the list of lists(two lists inside. first is french and other list is english)
 
 bitext = [[sentence.strip().split() for sentence in pair] for pair in islice(zip(open(f_data), open(e_data)), opts.num_sents)]
+
+
+#sys.stderr.write(str(bitext) + '\n')
 e_count = defaultdict(int)
 f_count = defaultdict(int)
 fe_count = defaultdict(int)
@@ -67,23 +73,54 @@ if verbose:
 
 k=0
 ## POPULATING THE T 
-while k<2 : 
+
+## PARAM FOR THE WORD-ALIGNMENT MODEL.
+smoothing=True
+NUM_ITERATIONS=1
+
+## If smoothing
+if smoothing:
+    sys.stderr.write('With Smoothing\n')
+V=10000 ## added vocab size; value borrowed from the research paper. 
+n=0.01 ## added count values borrowed from the research paper. 
+
+# Null value
+nullVal="<null>"
+
+while k<NUM_ITERATIONS : 
     if verbose:
         sys.stderr.write(' Iteration {} ..... \n'.format(k))
     k+=1
     for (n, (f, e)) in enumerate(bitext):
         for fi in f : 
             z=0
-            for ej in e:
-                z+=t[(fi,ej)] ## always going to be 1
+            # Include Null before anything else
+            z=t[(fi,nullVal)]
             for ej in e:
                 
+                z+=t[(fi,ej)] ## always going to be 1
+                
+            # Include Null before computing anything else
+            c=t[(fi,nullVal)]/z
+            fe_count[(fi,nullVal)]+=c
+            e_count[nullVal]+=c
+            for ej in e:
+                # if ej=='<null>' : 
+                #     c=t[(fi,ej)]/z
+                #     fe_count[(fi,ej)]*=c
+                #     e_count[ej]*=c
+                # else :
                 c=t[(fi,ej)]/z
                 fe_count[(fi,ej)]+=c
                 e_count[ej]+=c
 
     for (f,e) in fe_count : 
-        t[(f,e)]=fe_count[(f,e)]/e_count[e]
+        if smoothing:
+            ## smoothened observed counts
+            t[(f,e)]=( fe_count[(f,e)]+n )/( e_count[e]+(n*V) ) 
+        else:
+            ## Default baseline counts 
+            t[(f,e)]=fe_count[(f,e)]/e_count[e]
       
 
 #if verbose:
@@ -95,16 +132,28 @@ if verbose:
     sys.stderr.write('Finding best alignments\n')
 
 
+
+    
+
 ## FINDING THE BEST ALIGNMENT
 for (f, e) in bitext:
     for f,fi in enumerate(f)  :
         bestp=0
+        # Make Null the default bestp
+        bestp=t[(fi,nullVal)]
         bestj=0
         for j,ej in enumerate(e):
             if t[(fi,ej)]>bestp:
                 bestp=t[(fi,ej)]
                 bestj=j
-      
-        sys.stdout.write("%i-%i " % (f,bestj))
+        # ONLY OUTPUT IF bestj IS NOT 0 
+        # - i.e when bestj wasn't changed,
+        #       meaning t[(fi,ej)] wasn't better than t[(fi,nullVal)])
+        if bestj!=0:
+            sys.stdout.write("%i-%i " % (f,bestj))
+        #     sys.stderr.write('NULL ')
+        #     break
+        # else:
+        #     sys.stdout.write("%i-%i " % (f,bestj))
     sys.stdout.write("\n")
 
