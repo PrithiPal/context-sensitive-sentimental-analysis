ea61922d47a304f655c4116918474954ec3cba99
prithi_pal_singh_prithi_pal_singh@sfu.ca <pprithip@sfu.ca>
2018-12-10 06:24:32 +0000

    Added glove and yelp embeddings comparison; experimenting with large lexicon calculation

--- sa/constants.py
+++ sa/constants.py
@@ -3,8 +3,6 @@ import os
 from nltk.corpus import stopwords
 from pkg_resources import resource_filename
 
-### SYSTEM AGNOSTIC CONSTANTS 
-######
 DATA = os.path.abspath('embeddings_socialsent') + "/"
 print('data = ',DATA)
 PROCESSED_LEXICONS = 'lexicons_socialsent/'
@@ -13,38 +11,11 @@ STOPWORDS = set(stopwords.words('english'))
 LEXICON = 'inquirer'
 YEARS = map(str, range(1850, 2000, 10))
 
-######
-## THE FOLLOWING CAN BE REPLACED BY DOWNLOADING APPROPRIATE RESOURCES AND CHANGING PATHS:
-
-## GOOGLE EMBEDDING DOWNLOADED
 
 GOOGLE_EMBEDDINGS = './word2vec/GoogleNews-vectors-negative300.bin'
 GLOVE_EMBEDDINGS = './example_embeddings/glove.6B.100d.txt'
+YELP_EMBEDDINGS = './example_embeddings/yelp_w2v_pure'
 
 
 
-#from http://www.cis.lmu.de/~sascha/Ultradense/
-TWITTER_EMBEDDINGS = '/dfs/scratch0/gigawordvecs/twitter_lower_cw1_sg400_transformed.txt'
 
-
-
-# The following can be constructed from the corpora cited in the paper
-STOCK_EMBEDDINGS = '/lfs/madmax9/0/stock/svd-vecs'
-STOCK_COUNTS = '/lfs/madmax3/0/stock/vecs.bin'
-COHA_EMBEDDINGS = '/dfs/scratch0/COHA/cooccurs/word/ppmi/lsmooth0/nFalse/neg1/cdsTrue/svd/300/50000/'
-COHA_PPMI = '/dfs/scratch0/COHA/cooccurs/word/ppmi/lsmooth0/nFalse/neg1/cdsTrue/'
-COHA_COUNTS = '/dfs/scratch0/COHA/cooccurs/word/4/'
-COHA_SGNS_EMBEDDINGS = '/dfs/scratch0/COHA/cooccurs/word/sgns/300/'
-FREQS = "/dfs/scratch0/hist_words/coha-word/freqs.pkl"
-COHA_FREQS = "/dfs/scratch0/COHA/decade_freqs/{}-word.pkl"
-DFS_DATA = '/dfs/scratch0/googlengrams/eng-all/decades/'
-#POS = DFS_DATA + '/pos/'
-POS = "/dfs/scratch0/hist_words/coha-word/pos/"
-SUBREDDIT_EMBEDDINGS = '/dfs/scratch2/wleif/Reddit/vecs/{}/vecs'
-
-def make_directories():
-    socialsent_util.mkdir(DATA)
-    socialsent_util.mkdir(PROCESSED_LEXICONS)
-    socialsent_util.mkdir(POLARITIES)
-
-make_directories()
